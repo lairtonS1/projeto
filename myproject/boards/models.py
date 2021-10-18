e6e11de8ca17b0a8673ba1859ab6b638fedbdb99
Lailson Da Silva <Lailsonkazumi@gmail.com>
2021-10-18 08:13:03 -0300

    modelos definidos

---
+++ myproject/boards/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+
+class Board (models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Topic (models.Model):
+    subject = models.CharField(max_length=255)
+    last_update = models.DateTimeField(auto_now_add=True)
+    starter = models.ForeignKey(User, related_name='topics', on_delete=models.CASCADE)
+    board = models.ForeignKey(Board, related_name='topics', on_delete=models.CASCADE)
+
+
+class Post(models.Model):
+    message = models.TextField(max_length=4000)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(null=True)
+    created_by = models.ForeignKey(User, related_name='posts', on_delete=models.CASCADE)
+    updated_by = models.ForeignKey(User, related_name='+', null=True, on_delete=models.CASCADE)
