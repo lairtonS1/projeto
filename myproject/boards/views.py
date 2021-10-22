864bd9f8e90726498d4433782695b35b42e59a7d
Lairton da Silva <lairton20000309@gmail.com>
2021-10-22 18:16:09 -0300

    criado telas e formularios de login, função de redefinir senha adicionado, e telas de interação de post iniciado
    Signed-off-by: Lailson Da Silva <Lailsonkazumi@gmail.com>

--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -1,38 +1,46 @@
-from django.shortcuts import render, get_object_or_404,redirect
-from boards.models import Board,User,Topic,Post
-from django.http import  Http404
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404, redirect
+from boards.models import Board, User, Topic, Post
+from django.http import Http404
 from . import forms
+
+
 # Create your views here.
 
-
-def home(request):
+def home (request):
     response = Board.objetos.all()
     return render(request, "home.html", {"response": response})
-    
+
+@login_required(login_url='login')
 def board_topics(request, board_id):
     try:
         board = Board.objetos.get(id=board_id)
     except Board.DoesNotExist:
         raise Http404
-    return render(request, "board_topics.html", {"board":board})
+    return render(request, "board_topics.html", {"board": board})
 
-def new_topic (request, board_id):
-    board = get_object_or_404(Board,id=board_id)
+@login_required(login_url='login')
+def new_topic(request, board_id):
+    board = get_object_or_404(Board, id=board_id)
     user = User.objects.first()  # TODO: get the currently logged in user
     if request.method == 'POST':
         form = forms.NewTopicForm(request.POST)
         if form.is_valid():
             topic = form.save(commit=False)
             topic.board = board
-            topic.starter = user
+            topic.starter = request.user
             topic.save()
             post = Post.objetos.create(
                 message=form.cleaned_data.get('message'),
                 topic=topic,
-                created_by=user
+                created_by=request.user
             )
             return redirect('board_topics', board_id=board.id)  # TODO: redirect to the created topic page
     else:
         form = forms.NewTopicForm()
     return render(request, 'new_topic.html', {'board': board, 'form': form})
-    
+
+@login_required(login_url='login')
+def topic_posts(request,id,topics_id):
+    topic = get_object_or_404(Topic, board__id=id, id=topics_id)
+    return render (request, 'topic_posts.html',{'topic':topic})
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -16,26 +16,33 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from boards import views
-from accounts import  views as accounts_view
+from accounts import views as accounts_view
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('signup/', accounts_view.signup, name="signup" ),
+    path('signup/', accounts_view.signup, name="signup"),
     path('login/', auth_views.LoginView.as_view(template_name="login.html"), name='login'),
-    path('logout/',auth_views.LogoutView.as_view() ,name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('reset', auth_views.PasswordResetView.as_view(
-    template_name = 'password_reset.html',
-    email_template_name = 'password_reset_email.html',
-    subject_template_name = 'password_reset_subject.txt'
-    ), name='reset_password'),
+        template_name='password_reset.html',
+        email_template_name='password_reset_email.html',
+        subject_template_name='password_reset_subject.txt'
+    ), name='password_reset'),
     path('reset/done', auth_views.PasswordResetDoneView.as_view(
-    template_name = 'password_reset_done.html' ), name='password_reset_done'),
-    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confim.html')),
+        template_name='password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>',
+         auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'),
+         name='password_reset_confirm'),
     path("reset/complete", auth_views.PasswordResetCompleteView.as_view(
-    template_name = 'password_reset_complete.html'
-    )),
+        template_name='password_reset_complete.html'
+    ), name='password_reset_complete'),
+    path('settings/password', auth_views.PasswordChangeView.as_view(template_name='password_change.html'),
+         name='password_change'),
+    path('settings/password/done', auth_views.PasswordChangeDoneView.as_view(template_name='password_change_done.html'),
+         name='password_change_done'),
     path('admin/', admin.site.urls),
     path('', views.home, name="home"),
     path('board/<int:board_id>/', views.board_topics, name="board_topics"),
-    path("board/<int:board_id>/new/", views.new_topic, name="new_topic")
+    path("board/<int:board_id>/new/", views.new_topic, name="new_topic"),
+    path('board/<int:id>/topics/<int:topics_id>/', views.topic_posts, name="topic_posts")
 ]
